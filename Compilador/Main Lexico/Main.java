0f82334ebdc063c149f8e618504925440eae661c
Claudio Mota <cmoliveira1000@hotmail.com>
2014-08-06 17:12:20 -0300

    Projeto Final
    correções finais, testes casos positivos e negativos.

--- Compilador/Main Lexico/Main.java
+++ Compilador/Main Lexico/Main.java
@@ -15,7 +15,6 @@ public class Main {
    public static void main(String[] args) { 
       if (args.length > 0) { 
          try { 
-            /* Form our AST */ 
         	MyLexer lexer = new MyLexer (new PushbackReader(new FileReader(args[0]), 1024));
         	while(true)
         	{
